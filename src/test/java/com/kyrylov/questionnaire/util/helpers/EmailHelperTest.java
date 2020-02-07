1c18d41e5237216a0079cf7b6478a491a306941c
KyrylovDV <d.kyrylov@infostroy.com.ua>
2020-02-07 18:53:02 +0200

    Send email test

--- src/test/java/com/kyrylov/questionnaire/util/helpers/EmailHelperTest.java
+++ src/test/java/com/kyrylov/questionnaire/util/helpers/EmailHelperTest.java
@@ -1,7 +1,16 @@
 package com.kyrylov.questionnaire.util.helpers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.RepeatedTest;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 class EmailHelperTest {
 
+    @RepeatedTest(10)
+    void testSendEmail() throws IOException, MessagingException {
+        String email = ResourceHelper.getProperties(ResourceHelper.ResourceProperties.EMAIL_PROPERTIES)
+                .getProperty(ResourceHelper.ResourceProperties.EmailProperties.SMTPS_USER);
+        EmailHelper.sendEmail(email, "test", "test");
+    }
 }
